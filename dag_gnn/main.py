0a441fd3aff147dc15716ad23114134320dc3c39
an-seunghwan <dpeltms79@gmail.com>
2022-05-17 20:42:20 +0900

    update simulation and viz

--- dag_gnn/main.py
+++ dag_gnn/main.py
@@ -6,12 +6,24 @@ import numpy as np
 import pandas as pd
 
 import torch
+
+from utils.simulation import (
+    set_random_seed,
+    is_dag,
+    load_data,
+)
+
+from utils.viz import (
+    viz_graph,
+    viz_heatmap,
+)
 #%%
 config = {
+    "seed": 1,
     'data_type': 'synthetic', # discrete, real
     "n": 5000,
     "d": 5,
-    "degree": 2,
+    "degree": 3,
     "x_dim": 3,
     "graph_type": "ER",
     "sem_type": "gauss",
@@ -33,4 +45,35 @@ config = {
     "rho_max": 1e+16, 
     "rho_rate": 2.,
 }
+#%%
+import sys
+import subprocess
+try:
+    import wandb
+except:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "wandb"])
+    with open("../wandb_api.txt", "r") as f:
+        key = f.readlines()
+    subprocess.run(["wandb", "login"], input=key[0], encoding='utf-8')
+    import wandb
+
+wandb.init(
+    project="causal", 
+    entity="anseunghwan",
+    tags=["DAG-GNN", "nonlinear"],
+    # name='notears'
+)
+#%%
+set_random_seed(config["seed"])
+train_loader, W_true = load_data(config)
+
+wandb.run.summary['W_true'] = wandb.Table(data=pd.DataFrame(W_true))
+fig = viz_graph(W_true, size=(7, 7), show=True)
+wandb.log({'Graph': wandb.Image(fig)})
+fig = viz_heatmap(W_true, size=(5, 4))
+wandb.log({'heatmap': wandb.Image(fig)})
+#%%
+
+#%%
+wandb.run.finish()
 #%%
