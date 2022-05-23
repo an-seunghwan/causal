c1f0178f4346bc3d13c67179a1780a01d16903a0
an-seunghwan <dpeltms79@gmail.com>
2022-05-23 16:55:12 +0900

    update GAE folder

---
+++ gae/main.py
@@ -0,0 +1,273 @@
+#%%
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+#%%
+import numpy as np
+import pandas as pd
+import math
+import tqdm
+
+import torch
+
+from utils.simulation import (
+    set_random_seed,
+    is_dag,
+    load_data,
+    count_accuracy
+)
+
+from utils.viz import (
+    viz_graph,
+    viz_heatmap,
+)
+
+from utils.model import (
+    Encoder,
+    Decoder
+)
+#%%
+config = {
+    "seed": 42,
+    'data_type': 'synthetic', # discrete, real
+    "n": 5000,
+    "d": 10,
+    "degree": 2,
+    "x_dim": 1,
+    "graph_type": "ER",
+    "sem_type": "gauss",
+    "nonlinear_type": "nonlinear_2",
+    "hidden": 64,
+    
+    "epochs": 300,
+    "lr": 0.003,
+    "lr_decay": 200,
+    "gamma": 1.,
+    "batch_size": 100,
+    
+    "rho": 1, # initial value
+    "alpha": 0., # initial value
+    "h": np.inf, # initial value
+    
+    "max_iter": 100, 
+    "loss_type": 'l2',
+    "h_tol": 1e-8, 
+    "w_threshold": 0.3,
+    "lambda": 1e-8,
+    "progress_rate": 0.25,
+    "rho_max": 1e+20, 
+    "rho_rate": 10,
+    
+    "fig_show": True,
+}
+#%%
+import sys
+import subprocess
+try:
+    import wandb
+except:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "wandb"])
+    with open("../wandb_api.txt", "r") as f:
+        key = f.readlines()
+    subprocess.run(["wandb", "login"], input=key[0], encoding='utf-8')
+    import wandb
+
+wandb.init(
+    project="(causal)DAG-GNN", 
+    entity="anseunghwan",
+    tags=["nonlinear"],
+)
+#%%
+config["cuda"] = torch.cuda.is_available()
+
+set_random_seed(config["seed"])
+torch.manual_seed(config["seed"])
+if config["cuda"]:
+    torch.cuda.manual_seed(config["seed"])
+train_loader, W_true = load_data(config)
+
+wandb.run.summary['W_true'] = wandb.Table(data=pd.DataFrame(W_true))
+fig = viz_graph(W_true, size=(7, 7), show=config["fig_show"])
+wandb.log({'Graph': wandb.Image(fig)})
+fig = viz_heatmap(W_true, size=(5, 4), show=config["fig_show"])
+wandb.log({'heatmap': wandb.Image(fig)})
+#%%
+# """Generate off-diagonal interaction graph"""
+# off_diagonal = np.ones((config["d"], config["d"])) - np.eye(config["d"])
+# rel_rec = torch.DoubleTensor(np.array(encode_onehot(np.where(off_diagonal)[1]), dtype=np.float64))
+# rel_send = torch.DoubleTensor(np.array(encode_onehot(np.where(off_diagonal)[0]), dtype=np.float64))
+
+"""initialize adjacency matrix A"""
+adj_A = np.zeros((config["d"], config["d"]))
+#%%
+def h_fun(A, d):
+    x = torch.eye(d).float() + torch.div(A * A, d) # alpha = 1 / d
+    return torch.trace(torch.matrix_power(x, d)) - d
+#%%
+encoder = Encoder(config, adj_A, 32)
+decoder = Decoder(config, 32)
+
+if config["cuda"]:
+    encoder.cuda()
+    decoder.cuda()
+
+optimizer = torch.optim.Adam(
+    list(encoder.parameters()) + list(decoder.parameters()), 
+    lr=config["lr"]
+)
+torch.optim.lr_scheduler.StepLR(
+    optimizer, 
+    step_size=config["lr_decay"],
+    gamma=config["gamma"]
+)
+
+def update_optimizer(optimizer, lr, rho):
+    """related to lr to rho, whenever rho gets big, reduce lr propotionally"""
+    MAX_LR = 1e-2
+    MIN_LR = 1e-4
+    
+    estimated_lr = lr / (math.log10(rho) + 1e-10)
+    if estimated_lr > MAX_LR:
+        lr = MAX_LR
+    elif estimated_lr < MIN_LR:
+        lr = MIN_LR
+    else:
+        lr = estimated_lr
+    
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+    return optimizer, lr
+#%%
+def train(rho, alpha, h, config, optimizer):
+    encoder.train()
+    decoder.train()
+    
+    # update optimizer
+    optimizer, lr = update_optimizer(optimizer, config["lr"], rho)
+    
+    logs = {
+        'loss': [], 
+        'recon': [],
+        'kl': [],
+        'L1': [],
+        'aug': [],
+    }
+    
+    for batch_num, [train_batch] in enumerate(train_loader):
+        if config["cuda"]:
+            train_batch = train_batch.cuda()
+        
+        optimizer.zero_grad()
+        
+        logits, h, adj_A_amplified = encoder(train_batch)
+        recon, z = decoder(logits, adj_A_amplified, encoder.Wa)
+        
+        if torch.sum(adj_A_amplified != adj_A_amplified):
+            print('nan error\n')
+        if torch.sum(recon != recon):
+            print('nan error\n')
+            
+        loss_ = []    
+        
+        # reconstruction
+        recon = torch.pow(recon - train_batch, 2).sum() / train_batch.size(0)
+        loss_.append(('recon', recon))
+
+        # KL-divergence
+        kl = torch.pow(logits, 2).sum()
+        kl = 0.5 * kl / logits.size(0)
+        loss_.append(('kl', kl))
+
+        # sparsity loss
+        L1 = config["lambda"] * torch.sum(torch.abs(adj_A_amplified))
+        loss_.append(('L1', L1))
+
+        # augmentation and lagrangian loss
+        h_A = h_fun(adj_A_amplified, config["d"])
+        aug = 0.5 * rho * (h_A ** 2)
+        aug += alpha * h_A
+        """?????"""
+        aug += 100. * torch.trace(adj_A_amplified * adj_A_amplified)
+        loss_.append(('aug', aug))
+        
+        loss = sum([y for _, y in loss_])
+        loss_.append(('loss', loss))
+        
+        loss.backward()
+        optimizer.step()
+        
+        """accumulate losses"""
+        for x, y in loss_:
+            logs[x] = logs.get(x) + [y.item()]
+            
+    return logs, adj_A_amplified
+#%%
+rho = config["rho"]
+alpha = config["alpha"]
+h = config["h"]
+    
+for iteration in range(config["max_iter"]):
+    
+    """primal problem"""
+    while rho < config["rho_max"]:
+        # find argmin of primal problem (local solution) = update for config["epochs"] times
+        for epoch in tqdm.tqdm(range(config["epochs"]), desc="primal update"):
+            logs, adj_A_amplified = train(rho, alpha, h, config, optimizer)
+        # only one epoch is fine for finding argmin
+        # logs, adj_A_amplified = train(rho, alpha, h, config, optimizer)
+        
+        W_est = adj_A_amplified.data.clone()
+        h_new = h_fun(W_est, config["d"])
+        if h_new.item() > config["progress_rate"] * h:
+            rho *= config["rho_rate"]
+        else:
+            break
+    
+    """dual ascent"""
+    h = h_new.item()
+    alpha += config["rho"] * h_new.item()
+    
+    print_input = "[iteration {:03d}]".format(iteration)
+    print_input += ''.join([', {}: {:.4f}'.format(x, np.mean(y).round(2)) for x, y in logs.items()])
+    print_input += ', h(W): {:.8f}'.format(h)
+    print(print_input)
+    
+    """update log"""
+    wandb.log({x : np.mean(y) for x, y in logs.items()})
+    wandb.log({'h(W)' : h})
+    
+    """stopping rule"""
+    if h_new.item() <= config["h_tol"]:
+        break
+#%%
+"""final metrics"""
+adj_A_amplified = encoder.amplified_adjacency_matrix()
+W_est = adj_A_amplified.data.clone().numpy()
+W_est[np.abs(W_est) < config["w_threshold"]] = 0.
+W_est = W_est.astype(float).round(2)
+
+fig = viz_graph(W_est, size=(7, 7), show=config["fig_show"])
+wandb.log({'Graph_est': wandb.Image(fig)})
+fig = viz_heatmap(W_est, size=(5, 4), show=config["fig_show"])
+wandb.log({'heatmap_est': wandb.Image(fig)})
+
+wandb.run.summary['Is DAG?'] = is_dag(W_est)
+wandb.run.summary['W_est'] = wandb.Table(data=pd.DataFrame(W_est))
+wandb.run.summary['W_diff'] = wandb.Table(data=pd.DataFrame(W_true - W_est))
+
+W_ = (W_true != 0).astype(float)
+W_est_ = (W_est != 0).astype(float)
+W_diff_ = np.abs(W_ - W_est_)
+
+fig = viz_graph(W_diff_, size=(7, 7))
+wandb.log({'Graph_diff': wandb.Image(fig)})
+
+B_est = (W_est != 0).astype(float)
+B_true = (W_true != 0).astype(float)
+
+# compute metrics
+acc = count_accuracy(B_true, B_est)
+wandb.log(acc)
+#%%
+wandb.config.update(config)
+wandb.run.finish()
+#%%
---
+++ gae/utils/simulation.py
@@ -0,0 +1,334 @@
+#%%
+import torch
+from torch.utils.data.dataset import TensorDataset
+from torch.utils.data import DataLoader
+
+import numpy as np
+import random
+import igraph as ig
+#%%
+def sigmoid(z):
+    return 1. / (1. + np.exp(-z))
+#%%
+def set_random_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+#%%
+def is_dag(W: np.ndarray):
+    """check DAGness
+    Args:
+        W: weight matrix of graph
+    Returns:
+        Boolean: True if W is weight matrix of DAG
+    """
+    G = ig.Graph.Weighted_Adjacency(W.tolist())
+    return G.is_dag()
+#%%
+def simulate_dag(d: int, 
+                 degree: float, 
+                 graph_type: str,
+                 w_ranges: tuple = ((-2.0, -0.5), (0.5, 2.0))):
+    """Simulate random DAG with some expected number of edges
+    Args:
+        d (int): number of nodes
+        degree (float): expected node degree (= in + out)
+        graph_type (str): ER, SF
+        w_ranges (tuple): disjoint weight ranges
+    Returns:
+        W (np.ndarray): d x d weighted adjacency matrix of DAG
+    """
+    
+    def _random_permutation(M):
+        P = np.random.permutation(np.eye(M.shape[0]))
+        return P.T @ M @ P
+    
+    if graph_type == 'ER': # Erdos-Renyi
+        prob = float(degree) / (d - 1)
+        # select nodes which are goint to be connected (with probability prob)
+        B = np.tril((np.random.rand(d, d) < prob).astype(float), k=-1) # lower triangular
+    elif graph_type == 'SF': # Scale-free, Barabasi-Albert
+        m = int(round(degree / 2))
+        B = np.zeros([d, d])
+        bag = [0]
+        for i in range(1, d):
+            dest = np.random.choice(bag, size=m)
+            for j in dest:
+                B[i, j] = 1
+            bag.append(i)
+            bag.extend(dest)
+    else:
+        raise ValueError('unknown graph type')
+    
+    W = np.zeros(B.shape)
+    B_perm = _random_permutation(B)
+    assert ig.Graph.Adjacency(B_perm.tolist()).is_dag()
+    
+    S = np.random.randint(len(w_ranges), size=B.shape)  # choice of range
+    for i, (low, high) in enumerate(w_ranges):
+        U = np.random.uniform(low=low, high=high, size=B.shape)
+        W += B_perm * (S == i) * U
+    return W.round(2)
+#%%
+#%%
+def simulate_sem(W: np.ndarray, 
+                n: int, 
+                x_dim: int,
+                nonlinear_type: str = 'nonlinear_1',
+                sem_type: str = 'gauss', 
+                noise_scale=None):
+    """simulate samples from linear SEM with specified type of noise.
+    Args:
+        W (np.ndarray): d x d weighted adjacency matrix of DAG
+        n (int): number of samples
+        x_dim (int): dimension of each node variable
+        sem_type (str): gauss, exp, gumbel, uniform, logistic, poisson
+        nonlinear_type (str): nonlinear_1, nonlinear_2
+        noise_scale (float): scale parameter of addictive noise, default one
+    Returns:
+        X (np.ndarray): n x d sample matrix, 
+    """
+
+    def _simulate_single_equation(x, w, scale):
+        """
+        x: [n, num of parents]
+        w: [num of parents, ]
+        h: n x 1
+        """
+        
+        if nonlinear_type == 'nonlinear_1':
+            h = np.cos(x + 1).dot(w)
+        elif nonlinear_type == 'nonlinear_2':
+            h = 2. * np.sin((x + 0.5).dot(w)) + (x + 0.5).dot(w)
+        else:
+            raise ValueError('unknown nonlinear type')
+        
+        if sem_type == 'gauss':
+            z = scale * np.random.normal(size=n)
+            h += z
+        elif sem_type == 'exp':
+            z = np.random.exponential(scale=scale, size=n)
+            h += z
+        elif sem_type == 'gumbel':
+            z = np.random.gumbel(scale=scale, size=n)
+            h += z
+        elif sem_type == 'uniform':
+            z = np.random.uniform(low=-scale, high=scale, size=n)
+            h += z
+        elif sem_type == 'logistic':
+            h = np.random.binomial(1, sigmoid(h)) * 1.0
+        elif sem_type == 'poisson':
+            h = np.random.poisson(np.exp(h)) * 1.0
+        else:
+            raise ValueError('unknown sem type')
+        return h
+    
+    d = W.shape[0]
+    
+    # all node (variable) and dimensions share the same noise scale
+    if noise_scale is None:
+        scale = 1.
+    else:
+        if not np.isscalar(noise_scale):
+            raise ValueError('noise scale must be scalar!')
+        scale = noise_scale
+    
+    if not is_dag(W):
+        raise ValueError('W must be a DAG')
+    
+    G = ig.Graph.Weighted_Adjacency(W.tolist())
+    ordered_vertices = G.topological_sorting()
+    assert len(ordered_vertices) == d
+    
+    # first dimension for all node (variable)
+    X = np.zeros((n, d, x_dim))
+    for j in ordered_vertices:
+        parents = G.neighbors(j, mode=ig.IN)
+        X[:, j, 0] = _simulate_single_equation(X[:, parents, 0], W[parents, j], scale)
+    
+    # other dimenions
+    if x_dim > 1:
+        for i in range(1, x_dim):
+            X[:, :, i] = np.random.normal(scale=scale, size=1) * X[:, :, 0]
+            X[:, :, i] += np.random.normal(scale=scale, size=1)
+            X[:, :, i] += np.random.normal(scale=scale, size=(n, d))
+    X[:, :, 0] = np.random.normal(scale=scale, size=1) * X[:, :, 0]
+    X[:, :, 0] += np.random.normal(scale=scale, size=1)
+    X[:, :, 0] += np.random.normal(scale=scale, size=(n, d))
+    return X
+#%%
+def load_data(config):
+    W = simulate_dag(
+        config["d"],
+        config["degree"],
+        config["graph_type"],
+    )
+    X = simulate_sem(
+        W,
+        config["n"],
+        config["x_dim"],
+        config["nonlinear_type"],
+        config["sem_type"]
+    )
+    
+    X = torch.FloatTensor(X)
+    data = TensorDataset(X)
+    data_loader = DataLoader(data, batch_size=config["batch_size"])
+    return data_loader, W
+#%%
+def count_accuracy(B_true, B_est):
+    """Compute various accuracy metrics for B_est.
+    reverse = predicted association exists in condition in opposite direction
+    false positive = predicted association does not exist in condition
+    
+    Note that: Partial DAG is not considered!
+    
+    Args:
+        B_true (np.ndarray): d x d ground truth binary adjacency graph, {0, 1}
+        B_est (np.ndarray): d x d estimated binary adjacency graph, {0, 1}
+        
+    Returns:
+        FDR: (reverse + false positive) / prediction 
+        SHD: (undirected) extra + (undirected) missing + reverse
+        nonzero: prediction positive
+    """
+    
+    if not ((B_est == 0) | (B_est == 1)).all():
+        raise ValueError('B_est should take value in {0,1}')
+    if not is_dag(B_est):
+        raise ValueError('B_est should be a DAG')
+
+    d = B_true.shape[0]
+
+    # linear index of nonzero weights
+    pred = np.flatnonzero(B_est == 1)
+    cond = np.flatnonzero(B_true)
+    cond_reversed = np.flatnonzero(B_true.T)
+    cond_skeleton = np.concatenate([cond, cond_reversed])
+
+    # false pos
+    false_pos = np.setdiff1d(pred, cond_skeleton, assume_unique=True)
+    # reverse
+    extra = np.setdiff1d(pred, cond, assume_unique=True)
+    reverse = np.intersect1d(extra, cond_reversed, assume_unique=True)
+    # compute ratio
+    FDR = float(len(false_pos) + len(reverse)) / max(len(pred), 1)
+
+    # structural hamming distance
+    pred_lower = np.flatnonzero(np.tril(B_est + B_est.T))
+    cond_lower = np.flatnonzero(np.tril(B_true + B_true.T))
+    extra_lower = np.setdiff1d(pred_lower, cond_lower, assume_unique=True)
+    missing_lower = np.setdiff1d(cond_lower, pred_lower, assume_unique=True)
+    SHD = len(extra_lower) + len(missing_lower) + len(reverse)
+
+    return {'FDR': FDR, 'SHD': SHD, 'nonzero': len(pred)}
+#%%
+def main():
+    n = 100
+    d = 5
+    degree = 4
+    x_dim = 3
+    # graph_type = "ER"
+    # sem_type = "gauss"
+    # nonlinear_type = "nonlinear_2"
+    
+    for graph_type in ['ER', 'SF']:
+        W = simulate_dag(d, degree, graph_type)
+        assert is_dag(W)
+        assert W.shape == (d, d)
+    print('graph_type passed!\n')
+        
+    for sem_type in ['gauss', 'exp', 'gumbel', 'uniform', 'logistic', 'poisson']:
+        X = simulate_sem(W, n, x_dim, sem_type=sem_type)    
+        assert X.shape == (n, d, x_dim)
+    print('sem_type passed!\n')
+    
+    for nonlinear_type in ['nonlinear_1', 'nonlinear_2']:
+        X = simulate_sem(W, n, x_dim, nonlinear_type=nonlinear_type)
+        assert X.shape == (n, d, x_dim)
+    print('nonlinear_type passed!\n')
+        
+    noise_scale = np.ones((d, x_dim - 1))
+    try:
+        X = simulate_sem(W, n, x_dim, noise_scale=noise_scale)
+    except:
+        print('noise_scale passed!\n')
+#%%
+if __name__ == '__main__':
+    main()
+#%%
+"""FIXME"""
+# def simulate_sem(W: np.ndarray, 
+#                 n: int, 
+#                 x_dim: int,
+#                 nonlinear_type: str = 'nonlinear_1',
+#                 sem_type: str = 'gauss', 
+#                 noise_scale=None):
+#     """simulate samples from linear SEM with specified type of noise.
+#     Args:
+#         W (np.ndarray): d x d weighted adjacency matrix of DAG
+#         n (int): number of samples
+#         x_dim (int): dimension of each node variable
+#         sem_type (str): gauss, exp, gumbel, uniform, logistic, poisson
+#         nonlinear_type (str): nonlinear_1, nonlinear_2
+#         noise_scale (np.ndarray): scale parameter of addictive noise, default all ones
+#     Returns:
+#         X (np.ndarray): n x d sample matrix, 
+#     """
+
+#     def _simulate_single_equation(x, w, scale):
+#         """
+#         x: [n, num of parents, x_dim]
+#         w: [num of parents, 1]
+#         h: n x 1
+#         """
+#         if nonlinear_type == 'nonlinear_1':
+#             h = w.T @ np.cos(x + 1)
+#         elif nonlinear_type == 'nonlinear_2':
+#             h = 2. * np.sin(w.T @ (x + 0.5)) + w.T @ (x + 0.5)
+#         else:
+#             raise ValueError('unknown nonlinear type')
+        
+#         if sem_type == 'gauss':
+#             z = scale * np.random.normal(size=(n, 1, x_dim))
+#             h += z
+#         elif sem_type == 'exp':
+#             z = np.concatenate([np.random.exponential(scale=s, size=(n, 1, 1)) for s in scale], axis=-1)
+#             h += z
+#         elif sem_type == 'gumbel':
+#             z = np.concatenate([np.random.gumbel(scale=s, size=(n, 1, 1)) for s in scale], axis=-1)
+#             h += z
+#         elif sem_type == 'uniform':
+#             z = np.concatenate([np.random.uniform(low=-s, high=s, size=(n, 1, 1)) for s in scale], axis=-1)
+#             h += z
+#         elif sem_type == 'logistic':
+#             h = np.random.binomial(1, sigmoid(h)) * 1.0
+#         elif sem_type == 'poisson':
+#             h = np.random.poisson(np.exp(h)) * 1.0
+#         else:
+#             raise ValueError('unknown sem type')
+#         return h
+    
+#     d = W.shape[0]
+    
+#     if noise_scale is None:
+#         scale_vec = np.ones((d, x_dim))
+#     elif np.isscalar(noise_scale):
+#         scale_vec = noise_scale * np.ones((d, x_dim))
+#     else:
+#         if noise_scale.shape != (d, x_dim):
+#             raise ValueError('noise scale shape must be (d, x_dim)')
+#         scale_vec = noise_scale
+    
+#     if not is_dag(W):
+#         raise ValueError('W must be a DAG')
+    
+#     G = ig.Graph.Weighted_Adjacency(W.tolist())
+#     ordered_vertices = G.topological_sorting()
+#     assert len(ordered_vertices) == d
+    
+#     X = np.zeros((n, d, x_dim))
+#     for j in ordered_vertices:
+#         parents = G.neighbors(j, mode=ig.IN)
+#         X[:, [j], :] = _simulate_single_equation(X[:, parents], W[parents, j][..., None], scale_vec[j, :])
+#     return X
+#%%
