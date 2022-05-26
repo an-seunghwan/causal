0af3a9f20492d58531047dcefc46d96a45bbbe38
an-seunghwan <dpeltms79@gmail.com>
2022-05-26 21:49:26 +0900

    Update main.py

--- notears_mlp/main.py
+++ notears_mlp/main.py
@@ -56,7 +56,7 @@ import argparse
 def get_args(debug):
     parser = argparse.ArgumentParser('parameters')
 
-    parser.add_argument('--seed', type=int, default=5, 
+    parser.add_argument('--seed', type=int, default=10, 
                         help='seed for repeatable results')
     parser.add_argument('--n', default=200, type=int,
                         help='the number of dataset')
