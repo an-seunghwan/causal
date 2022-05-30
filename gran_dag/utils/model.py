68dfbf95dfd52557d7ff14eee0234ce8303ac88c
an-seunghwan <dpeltms79@gmail.com>
2022-05-30 19:13:31 +0900

    Update model.py

--- gran_dag/utils/model.py
+++ gran_dag/utils/model.py
@@ -63,98 +63,94 @@ class LocallyConnected(nn.Module):
             self.bias is not None
         )
 #%%
-class NotearsMLP(nn.Module):
-    def __init__(self, d, hidden_dims, bias=False):
-        super(NotearsMLP, self).__init__()
-        assert hidden_dims[-1] == 1
+class GraNDAG(nn.Module):
+    def __init__(self, d, hidden_dim, num_layers, bias=False):
+        super(GraNDAG, self).__init__()
         self.d = d
-        self.hidden_dims = hidden_dims
+        self.hidden_dims = hidden_dim
         
-        self.fc1 = nn.Linear(d, d * hidden_dims[0], bias=bias) # m1 = hidden_dims[0]
         layers = []
-        for i in range(len(hidden_dims)-1):
-            layers.append(LocallyConnected(d, hidden_dims[i], hidden_dims[i+1], bias=bias))
-        self.fc2 = nn.ModuleList(layers)
+        in_dim = d
+        out_dim = hidden_dim
+        for i in range(num_layers):
+            if i == num_layers - 1:
+                out_dim = 1 # dimension of each node == 1
+            layers.append(LocallyConnected(d, in_dim, out_dim, bias=bias))
+            if i == 0:
+                in_dim = hidden_dim
+        self.MLP = nn.ModuleList(layers)
+        
+        """masking layer"""
+        mask = torch.ones(d, d) - torch.eye(d)
+        self.mask = torch.stack([torch.diag(mask[i]) for i in range(d)], dim=0)
     
     def forward(self, x):
-        h = self.fc1(x)
-        h = h.reshape(-1, self.d, self.hidden_dims[0])
-        for layer in self.fc2:
-            h = torch.sigmoid(h)
+        x = x.unsqueeze(dim=1) 
+        h = x.repeat((1, self.d, 1)) # [n, d, d], m1 = d
+        h = torch.matmul(h.unsqueeze(dim=2), self.mask.unsqueeze(dim=0)) # [n, d, 1, d] = [n, d, 1, d] @ [1, d, d, d]
+        h = h.squeeze(dim=2)
+
+        for i, layer in enumerate(self.MLP):
             h = layer(h)
+            if i != self.num_layers - 1:
+                h = F.leaky_relu(h)
         h = h.squeeze(dim=2)
         return h
     
-    def h_func(self):
-        """Constrain L2-norm-squared of fc1 weights along m1 dim to be a DAG"""
-        W = self.fc1.weight.view(self.d, -1, self.d) # [i, m1, k]
-        W = torch.sum(W * W, axis=1) # [j, k]
-        W = W.t() # [k, j]
-        h = trace_expm(W) - self.d
-        return h
-    
-    def l2_reg(self):
-        """Take L2-norm-squared of all parameters"""
-        reg = 0.
-        reg += torch.sum(self.fc1.weight ** 2)
-        for layer in self.fc2:
-            reg += torch.sum(layer.weight ** 2)
-        return reg
-
-    def l1_reg(self):
-        """Take L2-norm of fc1 weight"""
-        return torch.sum(torch.abs(self.fc1.weight))
-    
     @torch.no_grad()
     def get_adjacency(self):
-        W = self.fc1.weight.view(self.d, -1, self.d) # [i, m1, k]
-        W = torch.sum(W * W, axis=1) # [j, k]
-        W = W.t() # [k, j]
-        W = torch.sqrt(W)
+        prod = self.mask # [d, d, d]
+        for weight in self.MLP.parameters():
+            if len(weight.shape) < 3: continue # bias
+            w = torch.abs(weight.unsqueeze(dim=0))
+            prod = torch.matmul(prod.unsqueeze(dim=2), w) # [d, d, 1, m2] = [d, d, 1, m1] @ [1, d, m1, m2]
+            prod = prod.squeeze(dim=2)
+        prod = torch.sum(prod, axis=-1) # [j, i]
+        W = prod.t() # adjacency matrix
         return W.cpu().detach().numpy()
 #%%
-d = config["d"]
-hidden_dim = config["hidden_dim"]
-num_layers = config["num_layers"]
+# d = config["d"]
+# hidden_dim = config["hidden_dim"]
+# num_layers = config["num_layers"]
 
-batch.shape
+# batch.shape
 
-"""without bias"""
-bias = True
-layers = []
-in_dim = d
-out_dim = hidden_dim
-for i in range(num_layers):
-    if i == num_layers - 1:
-        out_dim = 1 # dimension of each node == 1
-    layers.append(LocallyConnected(d, in_dim, out_dim, bias=bias))
-    if i == 0:
-        in_dim = hidden_dim
-MLP = nn.ModuleList(layers)
-#%%
-"""masking layer"""
-mask = torch.ones(d, d) - torch.eye(d)
-mask = torch.stack([torch.diag(mask[i]) for i in range(d)], dim=0)
+# """without bias"""
+# bias = True
+# layers = []
+# in_dim = d
+# out_dim = hidden_dim
+# for i in range(num_layers):
+#     if i == num_layers - 1:
+#         out_dim = 1 # dimension of each node == 1
+#     layers.append(LocallyConnected(d, in_dim, out_dim, bias=bias))
+#     if i == 0:
+#         in_dim = hidden_dim
+# MLP = nn.ModuleList(layers)
+# #%%
+# """masking layer"""
+# mask = torch.ones(d, d) - torch.eye(d)
+# mask = torch.stack([torch.diag(mask[i]) for i in range(d)], dim=0)
 
-x = batch.unsqueeze(dim=1) 
-h = x.repeat((1, d, 1)) # [n, d, d], m1 = d
-h = torch.matmul(h.unsqueeze(dim=2), mask.unsqueeze(dim=0)) # [n, d, 1, d] = [n, d, 1, d] @ [1, d, d, d]
-h = h.squeeze(dim=2)
-h.shape
+# x = batch.unsqueeze(dim=1) 
+# h = x.repeat((1, d, 1)) # [n, d, d], m1 = d
+# h = torch.matmul(h.unsqueeze(dim=2), mask.unsqueeze(dim=0)) # [n, d, 1, d] = [n, d, 1, d] @ [1, d, d, d]
+# h = h.squeeze(dim=2)
+# h.shape
 
-for i, layer in enumerate(MLP):
-    h = layer(h)
-    if i != config["num_layers"] - 1:
-        h = F.leaky_relu(h)
-h = h.squeeze(dim=2)
-h.shape
-#%%
-prod = mask # [d, d, d]
-for weight in MLP.parameters():
-    if len(weight.shape) < 3: continue # bias
-    w = torch.abs(weight.unsqueeze(dim=0))
-    prod = torch.matmul(prod.unsqueeze(dim=2), w) # [d, d, 1, m2] = [d, d, 1, m1] @ [1, d, m1, m2]
-    prod = prod.squeeze(dim=2)
-prod = torch.sum(prod, axis=-1) # [j, i]
-prod.t() # adjacency matrix
+# for i, layer in enumerate(MLP):
+#     h = layer(h)
+#     if i != config["num_layers"] - 1:
+#         h = F.leaky_relu(h)
+# h = h.squeeze(dim=2)
+# h.shape
+# #%%
+# prod = mask # [d, d, d]
+# for weight in MLP.parameters():
+#     if len(weight.shape) < 3: continue # bias
+#     w = torch.abs(weight.unsqueeze(dim=0))
+#     prod = torch.matmul(prod.unsqueeze(dim=2), w) # [d, d, 1, m2] = [d, d, 1, m1] @ [1, d, m1, m2]
+#     prod = prod.squeeze(dim=2)
+# prod = torch.sum(prod, axis=-1) # [j, i]
+# prod.t() # adjacency matrix
 #%%
